aa18c13f88c937feb7a9df33a0d06a6b2318755f
lineisha gibson <lineishagibson@gmail.com>
2022-12-19 17:30:32 -0600

    dragonClass

--- dragonstack/backend/index.js
+++ dragonstack/backend/index.js
@@ -1,1 +1,7 @@
-console.log("Hello Node");
+const Dragon = require('./dragon');
+
+const fooey = new Dragon(new Date(), 'fooey');
+const baloo = new Dragon(new Date(), 'baloo');
+
+console.log('fooey', fooey);
+console.log('baloo', baloo);
