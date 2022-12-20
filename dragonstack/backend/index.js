75f3c3cff2e6233bed279fd3fc0f1c4f888c5887
lineisha gibson <lineishagibson@gmail.com>
2022-12-20 14:28:24 -0600

    generations

--- dragonstack/backend/index.js
+++ dragonstack/backend/index.js
@@ -1,7 +1,23 @@
 const Dragon = require('./dragon');
 
-const fooey = new Dragon(new Date(), 'fooey');
-const baloo = new Dragon(new Date(), 'baloo');
+const fooey = new Dragon({
+    birthdate: new Date(),
+    nickname: 'fooey'
+});
+const baloo = new Dragon({
+    birthdate: new Date(),
+    nickname: 'baloo'
+});
+
+const yomomma = new Dragon();
+
+setTimeout(() => {
+    const gotcha = new Dragon();
+    console.log('gotcha',gotcha);
+}, 3000);
+
 
 console.log('fooey', fooey);
 console.log('baloo', baloo);
+console.log('yomomma', yomomma);
+
