cc3dfb39910c6afa5127d7fefc14c3fdfa0f4f21
lineisha gibson <lineishagibson@gmail.com>
2023-02-05 16:41:15 -0600

    avatars

--- dragonstack/frontend/src/components/DragonAvitar.js
+++ dragonstack/frontend/src/components/DragonAvitar.js
@@ -15,14 +15,33 @@ import { skinny, slender, sporty, stocky, striped, spotted, plain, patchy} from 
 
 
 class DragonAvitar extends Component {
+    get DragonImage(){
+        const dragonPropertyMap={};
+        this.props.dragon.traits.forEach(trait=>{
+            const{ traitType, traitValue}=trait;
+            dragonPropertyMap[traitType]= propertyMap[traitType][traitValue];
+        });
+        const {backgroundColor, build, pattern, size}= dragonPropertyMap;
+
+        const sizing= {width: size, height: size};
+        return(
+            <div className='dragon-avatar-image-wrapper'>
+                <div className='dragon-avatar-image-background' style={{backgroundColor, ...sizing}}></div>
+                <img src={pattern} className='dragon-avatar-image-pattern' style={{...sizing}}/>
+                <img src={build} className='dragon-avatar-image' style={{...sizing}}/>
+            </div>
+        )
+    }
     render(){
         const{generationId, dragonId, traits }= this.props.dragon;
+        if(!dragonId) return<div></div>;
+
         return(
             <div>
                 <span>G{generationId}. </span>
                 <span>I{dragonId}. </span>
-
                 {traits.map(trait =>trait.traitValue).join(', ') }
+                {this.DragonImage}
             </div>
         )
     }
