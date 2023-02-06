fbe8bf88fe66ca40116bbe354f3b73b8a4bce822
lineisha gibson <lineishagibson@gmail.com>
2023-02-05 18:51:12 -0600

    reduxExtension

--- dragonstack/frontend/src/index.js
+++ dragonstack/frontend/src/index.js
@@ -5,48 +5,22 @@ import {render} from 'react-dom';
 // import {Root} from "./Root";
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
+import {generationReducer} from './reducers';
+import {generationActionCreator} from './actions/generation';
+
+
 import './index.css';
 
 
-const DEFAULT_GENERATION = {generationId: '', expiration: ''};
-const  GENERATION_ACTION_TYPE = 'GENERATION_ACTION_TYPE';
 
-const generationReducer = (state, action)=>{
-    if(action.type === GENERATION_ACTION_TYPE){
-    return{generation: action.generation};
-}
-    return{ generation: DEFAULT_GENERATION};
-}
-
-const store = createStore(generationReducer);
+const store = createStore(generationReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 store.subscribe(()=>console.log('store state update', store.getState()));
-
-console.log('store', store);
-
-store.dispatch({type:'foo'});
-store.dispatch({type: GENERATION_ACTION_TYPE,
-generation: {generationId: 'goo', expiration: 'bar'}
-});
-
-console.log('store.getState()', store.getState());
-
-const generationActionCreator =(payload)=>{
-    return{
-        type: GENERATION_ACTION_TYPE,
-        generation: payload
-    };
-}
-const zooAction = generationActionCreator({
-    generationId: 'zoo', expiration: 'bar'
-});
-store.dispatch(zooAction);
 
 fetch('http://localhost:3000/generation')
     .then(response =>response.json())
     .then(json=>{
         store.dispatch(generationActionCreator(json.generation))
     });
-
 
 render(
     <div>
